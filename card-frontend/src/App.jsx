18ab52c9d3cd99d85ffd68d520d970973967da1e
Jacob Templeton <jacob.templeton@gmail.com>
2023-01-31 14:01:18 -0800

    Add form to submit new cards, state to handle the new card data and event handler to store new card data

--- card-frontend/src/App.jsx
+++ card-frontend/src/App.jsx
@@ -2,10 +2,81 @@ import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  width: "25rem",
+  margin: "0 auto 4rem auto",
+  gap: "2rem",
+  border: "1px solid white",
+  padding: "2rem",
+  borderRadius: "5px",
+};
+
 function App({ cards }) {
   const [cardData, setCardData] = useState(cards);
+  const [newCard, setNewCard] = useState({
+    company: "",
+    description: "",
+    notes: "",
+    service: "",
+  });
+
+  const addNewCard = (event) => {
+    event.preventDefault();
+    const newCardObject = {
+      company: newCard.company,
+      description: newCard.description,
+      notes: newCard.notes,
+      service: newCard.service,
+      status: "Pending",
+      id: cardData.length + 1,
+    };
+  };
+  const handleNewCard = (event) => {
+    const { name, value } = event.target;
+    setNewCard((prevCard) => {
+      return { ...prevCard, [name]: value };
+    });
+  };
+
   return (
     <div className="App">
+      <form style={styles} onSubmit={addNewCard}>
+        <input
+          type="text"
+          placeholder="Company"
+          name="company"
+          value={newCard.company}
+          onChange={handleNewCard}
+        />
+        <textarea
+          placeholder="Description"
+          name="description"
+          value={newCard.description}
+          onChange={handleNewCard}
+        />
+        <textarea
+          placeholder="Notes"
+          name="notes"
+          value={newCard.notes}
+          onChange={handleNewCard}
+        />
+        <label htmlFor="service">Service Level</label>
+        <select
+          id="service"
+          value={newCard.service}
+          name="service"
+          onChange={handleNewCard}
+        >
+          <option value="walk-through">Walk Through</option>
+          <option value="super-express">Super-Express</option>
+          <option value="express">Express</option>
+          <option value="regular">Regular</option>
+          <option value="value">Value</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
       <div>
         {cardData.map((card) => {
           return <Card card={card} />;
