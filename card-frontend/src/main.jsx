17d190528044da62fef1343c26e56085cea6bf1c
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-01 12:44:35 -0800

    Add proxy settings to vite config

--- card-frontend/src/main.jsx
+++ card-frontend/src/main.jsx
@@ -3,33 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// const cards = [
-//   {
-//     id: 1,
-//     company: "Topps",
-//     description: "Curry rookie Card",
-//     notes: "Autographed",
-//     service: "Super Express",
-//     status: "Pending",
-//   },
-//   {
-//     id: 2,
-//     company: "Fleer",
-//     description: "Durant rookie Card",
-//     notes: null,
-//     service: "Express",
-//     status: "Pending",
-//   },
-//   {
-//     id: 3,
-//     company: "Panini",
-//     description: "Doncic rookie Card",
-//     notes: "Autographed",
-//     service: "Walk Through",
-//     status: "Pending",
-//   },
-// ];
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
