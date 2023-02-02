d9a157a8d245bdf7b42b4f397fea216ab3e47549
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-01 16:21:43 -0800

    Add get and post requests that use the mongo database

--- card-backend/index.js
+++ card-backend/index.js
@@ -1,45 +1,47 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Card = require("./models/card");
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
 
-let cards = [
-  {
-    id: 1,
-    company: "Topps",
-    description: "Curry rookie Card",
-    notes: "Autographed",
-    service: "Super Express",
-    status: "Pending",
-  },
-  {
-    id: 2,
-    company: "Fleer",
-    description: "Durant rookie Card",
-    notes: null,
-    service: "Express",
-    status: "Pending",
-  },
-  {
-    id: 3,
-    company: "Panini",
-    description: "Doncic rookie Card",
-    notes: "Autographed",
-    service: "Walk Through",
-    status: "Pending",
-  },
-  {
-    company: "Topps",
-    description: "LeBron Card",
-    notes: null,
-    service: "Walk-through",
-    status: "Pending",
-    id: 4,
-  },
-];
+// let cards = [
+//   {
+//     id: 1,
+//     company: "Topps",
+//     description: "Curry rookie Card",
+//     notes: "Autographed",
+//     service: "Super Express",
+//     status: "Pending",
+//   },
+//   {
+//     id: 2,
+//     company: "Fleer",
+//     description: "Durant rookie Card",
+//     notes: null,
+//     service: "Express",
+//     status: "Pending",
+//   },
+//   {
+//     id: 3,
+//     company: "Panini",
+//     description: "Doncic rookie Card",
+//     notes: "Autographed",
+//     service: "Walk Through",
+//     status: "Pending",
+//   },
+//   {
+//     company: "Topps",
+//     description: "LeBron Card",
+//     notes: null,
+//     service: "Walk-through",
+//     status: "Pending",
+//     id: 4,
+//   },
+// ];
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -55,42 +57,50 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/cards", (request, response) => {
-  response.json(cards);
+  Card.find({}).then((cards) => {
+    response.json(cards);
+  });
 });
 
 app.get("/api/cards/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const card = cards.find((card) => card.id === id);
-  if (card) {
-    response.json(card);
-  } else {
-    response.status(404).end();
-  }
+  Card.findById(request.params.id)
+    .then((card) => {
+      if (card) {
+        response.json(card);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 
-const generateId = () => {
-  const maxId = cards.length > 0 ? Math.max(...cards.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+// const generateId = () => {
+//   const maxId = cards.length > 0 ? Math.max(...cards.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
 
 app.post("/api/cards", (request, response) => {
+  const body = request.body;
   if (!body.company || !body.description) {
     return response.status(400).json({
       error: "company or description missing",
     });
   }
-  const card = {
-    id: generateId(),
+  const card = new Card({
     submitted: new Date(),
     company: body.company,
     description: body.description,
     notes: body.notes || null,
     service: body.service,
     status: body.status,
-  };
+  });
 
-  cards = cards.concat(card);
-  response.json(card);
+  card.save().then((savedCard) => {
+    response.json(savedCard);
+  });
 });
 
 app.delete("/api/cards/:id", (request, response) => {
@@ -106,7 +116,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ card-backend/models/card.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const cardSchema = new mongoose.Schema({
+  company: String,
+  description: String,
+  notes: String,
+  service: String,
+  received: Date,
+  status: String,
+});
+
+cardSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Card", cardSchema);
