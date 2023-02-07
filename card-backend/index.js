7e537a93c7f0ada06a110214b0fcbd63296bf121
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-06 17:56:48 -0800

    Add new test that initializes the test database before the tests are run

--- card-backend/index.js
+++ card-backend/index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
+const config = require("./utils/config");
 
 const logger = require("./utils/logger");
-const config = require("./utils/config");
 
 // let cards = [
 //   {
--- card-backend/tests/card_api.test.js
+++ card-backend/tests/card_api.test.js
@@ -4,6 +4,35 @@ const app = require("../app");
 
 const api = supertest(app);
 
+const Card = require("../models/card");
+
+const initialCards = [
+  {
+    company: "Panini",
+    description: "Curry Rookie Card",
+    notes: "Autographed",
+    service: "Walk-Through",
+    submitted: new Date(),
+    status: "Pending",
+  },
+  {
+    company: "Topps",
+    description: "Durant Rookie Card",
+    notes: null,
+    service: "Super-Express",
+    submitted: new Date(),
+    status: "Pending",
+  },
+];
+
+beforeEach(async () => {
+  await Card.deleteMany({});
+  let cardObject = new Card(initialCards[0]);
+  await cardObject.save();
+  cardObject = new Card(initialCards[1]);
+  await cardObject.save();
+});
+
 test("cards are returned as json", async () => {
   await api
     .get("/api/cards")
@@ -11,6 +40,19 @@ test("cards are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
+test("all Cards returned", async () => {
+  const response = await api.get("/api/cards");
+
+  expect(response.body).toHaveLength(initialCards.length);
+});
+
+test("a specific card is contained in the returned cards", async () => {
+  const response = await api.get("/api/cards");
+
+  const contents = response.body.map((card) => card.description);
+  expect(contents).toContain("Curry Rookie Card");
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
-}, 100000);
+});
