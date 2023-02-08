07d299b61241237440b80be4638bf5b8ac4ddf77
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-07 16:29:20 -0800

    Add helper function that returns the cards saved in the DB and contains the intial cards saved in the DB at the beginning of the testing

--- card-backend/tests/card_api.test.js
+++ card-backend/tests/card_api.test.js
@@ -1,35 +1,17 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
 
 const api = supertest(app);
 
 const Card = require("../models/card");
 
-const initialCards = [
-  {
-    company: "Panini",
-    description: "Curry Rookie Card",
-    notes: "Autographed",
-    service: "Walk-Through",
-    submitted: new Date(),
-    status: "Pending",
-  },
-  {
-    company: "Topps",
-    description: "Durant Rookie Card",
-    notes: null,
-    service: "Super-Express",
-    submitted: new Date(),
-    status: "Pending",
-  },
-];
-
 beforeEach(async () => {
   await Card.deleteMany({});
-  let cardObject = new Card(initialCards[0]);
+  let cardObject = new Card(helper.initialCards[0]);
   await cardObject.save();
-  cardObject = new Card(initialCards[1]);
+  cardObject = new Card(helper.initialCards[1]);
   await cardObject.save();
 });
 
@@ -43,7 +25,7 @@ test("cards are returned as json", async () => {
 test("all Cards returned", async () => {
   const response = await api.get("/api/cards");
 
-  expect(response.body).toHaveLength(initialCards.length);
+  expect(response.body).toHaveLength(helper.initialCards.length);
 });
 
 test("a specific card is contained in the returned cards", async () => {
@@ -69,11 +51,12 @@ test("A valid card can be added", async () => {
     .expect(201)
     .expect("Content-Type", /application\/json/);
 
-  const response = await api.get("/api/cards");
-  const description = response.body.map((card) => card.description);
+  const cardsAtEnd = await helper.cardsInDb();
 
-  expect(response.body).toHaveLength(initialCards.length + 1);
-  expect(description).toContain("Doncic Rookie Card");
+  expect(cardsAtEnd).toHaveLength(helper.initialCards.length + 1);
+
+  const descriptions = cardsAtEnd.map((card) => card.description);
+  expect(descriptions).toContain("Doncic Rookie Card");
 });
 
 test("A car without a description is not added", async () => {
@@ -88,9 +71,9 @@ test("A car without a description is not added", async () => {
 
   await api.post("/api/cards").send(newCard).expect(400);
 
-  const response = await api.get("/api/cards");
+  const cardsAtEnd = await helper.cardsInDb();
 
-  expect(response.body).toHaveLength(initialCards.length);
+  expect(cardsAtEnd).toHaveLength(helper.initialCards.length);
 });
 
 afterAll(async () => {
---
+++ card-backend/tests/test_helper.js
@@ -0,0 +1,48 @@
+const Card = require("../models/card");
+
+const initialCards = [
+  {
+    company: "Panini",
+    description: "Curry Rookie Card",
+    notes: "Autographed",
+    service: "Walk-Through",
+    submitted: new Date(),
+    status: "Pending",
+  },
+  {
+    company: "Topps",
+    description: "Durant Rookie Card",
+    notes: null,
+    service: "Super-Express",
+    submitted: new Date(),
+    status: "Pending",
+  },
+];
+
+const nonExistingId = async () => {
+  const card = new Card({
+    company: "Panini",
+    description: "Test Rookie Card",
+    notes: "Autographed",
+    service: "Walk-Through",
+    submitted: new Date(),
+    status: "Pending",
+  });
+
+  await card.save();
+  await card.remove();
+
+  return card._id.toString();
+};
+
+const cardsInDb = async () => {
+  const cards = await Card.find({});
+
+  return cards.map((card) => card.toJSON());
+};
+
+module.exports = {
+  initialCards,
+  nonExistingId,
+  cardsInDb,
+};
