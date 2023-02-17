ed47bb25cd338918a35d7e0b0404e939f315e80d
Jacob Templeton <jacob.templeton@gmail.com>
2023-02-16 21:29:20 -0800

    Add extract login form and submit card form into their own functions, conditionally render them to show login form if nobody is logged in and to show the card submit form if somebody is logged in

--- card-frontend/src/App.jsx
+++ card-frontend/src/App.jsx
@@ -76,67 +76,80 @@ function App({ cards }) {
     });
   };
 
-  return (
-    <div className="App">
-      <form onSubmit={handleLogin}>
-        <div>
-          {" "}
-          username{" "}
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />{" "}
-        </div>
-        <div>
-          {" "}
-          password{" "}
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />{" "}
-        </div>
-        <button type="submit">login</button>
-      </form>
-      <form style={styles} onSubmit={addNewCard}>
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        {" "}
+        username{" "}
         <input
           type="text"
-          placeholder="Company"
-          name="company"
-          value={newCard.company}
-          onChange={handleNewCard}
-        />
-        <textarea
-          placeholder="Description"
-          name="description"
-          value={newCard.description}
-          onChange={handleNewCard}
-        />
-        <textarea
-          placeholder="Notes"
-          name="notes"
-          value={newCard.notes}
-          onChange={handleNewCard}
-        />
-        <label htmlFor="service">Service Level</label>
-        <select
-          id="service"
-          value={newCard.service}
-          name="service"
-          onChange={handleNewCard}
-        >
-          <option>--Select a service--</option>
-          <option value="Walk-through">Walk Through</option>
-          <option value="Super-Express">Super-Express</option>
-          <option value="Express">Express</option>
-          <option value="Regular">Regular</option>
-          <option value="Value">Value</option>
-        </select>
-        <button type="submit">Submit</button>
-      </form>
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />{" "}
+      </div>
+      <div>
+        {" "}
+        password{" "}
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />{" "}
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const cardForm = () => (
+    <form style={styles} onSubmit={addNewCard}>
+      <input
+        type="text"
+        placeholder="Company"
+        name="company"
+        value={newCard.company}
+        onChange={handleNewCard}
+      />
+      <textarea
+        placeholder="Description"
+        name="description"
+        value={newCard.description}
+        onChange={handleNewCard}
+      />
+      <textarea
+        placeholder="Notes"
+        name="notes"
+        value={newCard.notes}
+        onChange={handleNewCard}
+      />
+      <label htmlFor="service">Service Level</label>
+      <select
+        id="service"
+        value={newCard.service}
+        name="service"
+        onChange={handleNewCard}
+      >
+        <option>--Select a service--</option>
+        <option value="Walk-through">Walk Through</option>
+        <option value="Super-Express">Super-Express</option>
+        <option value="Express">Express</option>
+        <option value="Regular">Regular</option>
+        <option value="Value">Value</option>
+      </select>
+      <button type="submit">Submit</button>
+    </form>
+  );
+
+  return (
+    <div className="App">
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          {cardForm()}{" "}
+        </div>
+      )}
       <div>
         {cardData.map((card) => {
           return <Card card={card} key={card.id} />;
